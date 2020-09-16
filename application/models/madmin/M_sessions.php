91997fd8eebb0282af7150f3f38fce86b5d6c3ff
vishalsavaliya <vishalsavaliya3@gmail.com>
2020-09-16 09:42:38 +0530

    Update json

--- application/models/madmin/M_sessions.php
+++ application/models/madmin/M_sessions.php
@@ -948,11 +948,7 @@ class M_sessions extends CI_Model {
                 'questions' => $questions,
                 'charting' => $charting
             );
-
-            echo "<pre>";
-            print_r(json_encode($create_array));
-            die;
-			
+           
             $json_array = array("data" => json_encode($create_array), "session_reference" => (int) $result_sessions->sessions_id, "session_id" => (int) $result_sessions->sessions_id, "source" => "gravity");
 
             $data_to_post = "data=" . json_encode($create_array) . "&session_reference=" . (int) $result_sessions->sessions_id . "&session_id=" . (int) $result_sessions->sessions_id . "&source=gravity"; //if http_build_query causes any problem with JSON data, send this parameter directly in post.
