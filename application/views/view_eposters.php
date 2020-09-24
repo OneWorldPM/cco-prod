023b721ffac0a4d7e4340b85e7f19e503e9e2ff3
vishalsavaliya <vishalsavaliya3@gmail.com>
2020-09-24 15:59:21 +0530

    Send Json Update 24-9-2020

--- application/views/view_eposters.php
+++ application/views/view_eposters.php
@@ -16,18 +16,18 @@
                     <section class="content">
                         <div class="container" style=" background: rgba(250, 250, 250, 0.8);"> 
                             <div class="row p-b-40">
-                                <div class="col-md-12" style="background-color: #B2B7BB; margin-bottom: 10px;">
+                                <div class="col-md-12" style="background-color: #B2B7BB; margin-bottom: 10px; ">
                                     <h3 style="margin-bottom: 5px; color: #fff; font-weight: 700; text-transform: uppercase;"><?= $eposters->eposters_title ?></h3>
                                 </div>
-                                <div class="col-md-12">
-                                    <div class="col-md-1" style="text-align: center;">
+                                <div class="col-md-12" style="padding-left: 0px; padding-right: 0px">
+                                    <div class="col-md-1" style="text-align: center; padding-left: 0px; padding-right: 0px">
                                         <a href="<?= base_url() ?>eposters/previous/<?= isset($eposters) ? $eposters->eposters_id : "" ?>"><i class="fa fa-angle-left" style="font-size: 8pc; margin-top: 350px;"></i></a>
                                     </div>
-                                    <div class="col-md-10" style="text-align: center;">
-                                        <img src="<?= base_url() ?>uploads/eposters/<?= isset($eposters) ? $eposters->eposters_area_photo : "" ?>" style="height: 800px; width:100%;"/>
+                                    <div class="col-md-10" style="text-align: center; padding-left: 0px; padding-right: 0px">
+                                        <img src="<?= base_url() ?>uploads/eposters/<?= isset($eposters) ? $eposters->eposters_area_photo : "" ?>" width="100%"/>
                                         <a href="<?= base_url() ?>eposters" class="button btn small" style="background-color: #c3c3c3; border-color: #c3c3c3; font-size: 20px; text-transform: unset;"><span>Return to ePoster Listing</span></a>
                                     </div>
-                                    <div class="col-md-1" style="text-align: center;">
+                                    <div class="col-md-1" style="text-align: center; padding-left: 0px; padding-right: 0px">
                                         <a href="<?= base_url() ?>eposters/next/<?= isset($eposters) ? $eposters->eposters_id : "" ?>"><i class="fa fa-angle-right" style="font-size: 8pc; margin-top: 350px;"></i></a>
                                     </div>
                                 </div>
