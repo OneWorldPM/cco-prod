b621c26a85e2927654a2596802552264196c2972
vishalsavaliya <vishalsavaliya3@gmail.com>
2020-11-09 18:00:11 +0530

    json update

--- application/controllers/Sessions.php
+++ application/controllers/Sessions.php
@@ -281,9 +281,9 @@ class Sessions extends CI_Controller {
                 if (date("Y-m-d H:i:s", strtotime($sessions_details->sessions_date . ' ' . $sessions_details->end_time)) < date("Y-m-d H:i:s")) {
                     $end_date_time = date("Y-m-d H:i:s", strtotime($sessions_details->sessions_date . ' ' . $sessions_details->end_time));
                 } else {
-                    if (date("Y-m-d H:i:s", strtotime($login_sessions_history->start_date_time)) > date("Y-m-d H:i:s")) {
+                   if (date("Y-m-d H:i:s", strtotime($login_sessions_history->start_date_time)) > date("Y-m-d H:i:s")) {
                         $end_date_time = date("Y-m-d H:i:s", strtotime($login_sessions_history->start_date_time));
-                    } else if (date("Y-m-d H:i:s") > date("Y-m-d H:i:s", strtotime($sessions_details->sessions_date . ' ' . $sessions_details->end_time))) {{
+                    } else if (date("Y-m-d H:i:s") > date("Y-m-d H:i:s", strtotime($sessions_details->sessions_date . ' ' . $sessions_details->end_time))) {
                         $end_date_time = date("Y-m-d H:i:s", strtotime($sessions_details->sessions_date . ' ' . $sessions_details->end_time));
                     } else {
                         $end_date_time = date("Y-m-d H:i:s");
