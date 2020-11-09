a042b44be99458bbee091779ede3dc44d4ba1d17
vishalsavaliya <vishalsavaliya3@gmail.com>
2020-11-09 16:34:47 +0530

    json total time Fix

--- application/controllers/Sessions.php
+++ application/controllers/Sessions.php
@@ -232,8 +232,12 @@ class Sessions extends CI_Controller {
 //            );
 //            $this->db->update('login_sessions_history', $session_his_arr, array("login_sessions_history_id" => $login_sessions_history->login_sessions_history_id));
         } else {
-             if (date("Y-m-d H:i:s", strtotime($sessions_details->sessions_date . ' ' . $sessions_details->time_slot)) < date("Y-m-d H:i:s") && date("Y-m-d H:i:s") < date("Y-m-d H:i:s", strtotime($sessions_details->sessions_date . ' ' . $sessions_details->end_time))) {
-                $start_date_time = date("Y-m-d H:i:s");
+               if (date("Y-m-d H:i:s", strtotime($sessions_details->sessions_date . ' ' . $sessions_details->time_slot)) < date("Y-m-d H:i:s") && date("Y-m-d H:i:s") < date("Y-m-d H:i:s", strtotime($sessions_details->sessions_date . ' ' . $sessions_details->end_time))) {
+                if (date("Y-m-d H:i:s") < date("Y-m-d H:i:s", strtotime($sessions_details->sessions_date . ' ' . $sessions_details->end_time))) {
+                    $start_date_time = date("Y-m-d H:i:s");
+                } else {
+                    $start_date_time = date("Y-m-d H:i:s", strtotime($sessions_details->sessions_date . ' ' . $sessions_details->end_time));
+                }
             } else {
                 $start_date_time = date("Y-m-d H:i:s", strtotime($sessions_details->sessions_date . ' ' . $sessions_details->time_slot));
             }
