b1c29e585dcc088b69c2e36d5b53f5cb9e7e9e07
vishalsavaliya <vishalsavaliya3@gmail.com>
2020-11-11 09:08:01 +0530

    End Session page - edits

--- application/views/end_session.php
+++ application/views/end_session.php
@@ -21,7 +21,7 @@
     }
 
 </style>
-<section class="parallax" style="background-image: url(<?= base_url() ?>front_assets/images/attend_background.png); top: 0; padding-top: 0px;">
+<section class="parallax fullscreen" style="background-image: url(<?= base_url() ?>front_assets/images/attend_background.png); top: 0; padding-top: 0px;">
 <div class="container container-fullscreen">
     <div class="jumbotron">
         <h2>This session is now closed.</h2>
