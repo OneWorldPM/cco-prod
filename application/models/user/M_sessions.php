0673952dcfef7352e06e5c2e8ad9f0f44941b044
athulak <athullive@gmail.com>
2020-12-03 01:08:22 +0100

    Session listing bug fix

--- application/models/user/M_sessions.php
+++ application/models/user/M_sessions.php
@@ -29,7 +29,7 @@ class M_sessions extends CI_Model {
         $this->db->select("s.sessions_date,DAYNAME(s.sessions_date) as dayname");
         $this->db->from("sessions s");
         $this->db->where('DATE_FORMAT(s.sessions_date, "%Y-%m-%d") >=', date('Y-m-d'));
-        $this->db->where('DATE_FORMAT(s.sessions_date, "%Y-%m-%d") <=', date('Y-m-d', strtotime("+7 days")));
+        $this->db->where('DATE_FORMAT(s.sessions_date, "%Y-%m-%d") <', date('Y-m-d', strtotime("+7 days")));
         $this->db->group_by('dayname');
         $this->db->order_by("s.sessions_date", "asc");
         $result = $this->db->get();
