1186c450df9fe1b5ba13c4ce1dbddf13a5b56477
Rexter Dayuta <rexterdayuta@gmail.com>
2021-07-26 22:26:32 +0800

    js ver

--- application/views/presenter/view_session.php
+++ application/views/presenter/view_session.php
@@ -236,7 +236,7 @@ else{
     });
 </script>
 <!-- Please add scripts only in this JS file, NOT directly on this HTML file -->
-<script src="<?= base_url() ?>front_assets/presenter/view_session.js?v=19"></script>
+<script src="<?= base_url() ?>front_assets/presenter/view_session.js?v=20"></script>
 
 <script>
     window.onscroll = function() {myFunction()};
