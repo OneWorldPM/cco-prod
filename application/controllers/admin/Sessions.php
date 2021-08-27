5661a5ed51e890a43185e0307d04eafaee5fde9d
Rexter Dayuta <rexterdayuta@gmail.com>
2021-08-28 03:54:40 +0800

    Revert "dynamin fixed moderators"
    This reverts commit 4af37ae1555afca04f087c941d464350f1598d64.

--- application/controllers/admin/Sessions.php
+++ application/controllers/admin/Sessions.php
@@ -57,7 +57,6 @@ class Sessions extends CI_Controller {
         $data['session_tracks'] = $this->msessions->getSessionTracks();
         $data['unique_identifier_id'] = $this->msessions->getSession_Unique_Identifier_ID();
         $data['millicast_stream_names']=$this->msessions->getMillicast_Stream_Name();
-        $data['default_moderators']=$this->msessions->moderatorCheckedList();
         $this->load->view('admin/header');
         $this->load->view('admin/add_sessions', $data);
         $this->load->view('admin/footer');
@@ -1068,25 +1067,6 @@ public function archive_session() {
             echo 0;
 
         return;
-    }
-
-    public function moderatorCheckedList(){
-        $this->load->view('admin/header');
-        $this->load->view('admin/moderatorCheckedList');
-        $this->load->view('admin/footer');
-    }
-
-    public function getPresentersJson(){
-        echo json_encode($this->msessions->getPresenters());
-//        echo json_encode($this->msessions->getSelectedModerators());
-    }
-
-    public function getSelectedModerators(){
-        echo json_encode($this->msessions->getSelectedModerators());
-    }
-
-    public function addSelectedModerator(){
-        echo json_encode($this->msessions->addSelectedModerator());
     }
 
 }
