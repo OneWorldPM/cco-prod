1c16b81b219edc9989e93250229e0b545814da30
Rexter Dayuta <rexterdayuta@gmail.com>
2021-08-28 04:08:37 +0800

    dynamin fixed moderators

--- application/controllers/admin/Sessions.php
+++ application/controllers/admin/Sessions.php
@@ -57,6 +57,7 @@ class Sessions extends CI_Controller {
         $data['session_tracks'] = $this->msessions->getSessionTracks();
         $data['unique_identifier_id'] = $this->msessions->getSession_Unique_Identifier_ID();
         $data['millicast_stream_names']=$this->msessions->getMillicast_Stream_Name();
+        $data['default_moderators']=$this->msessions->moderatorCheckedList();
         $this->load->view('admin/header');
         $this->load->view('admin/add_sessions', $data);
         $this->load->view('admin/footer');
