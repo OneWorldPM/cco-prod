71f557160ae2842acf135c279d6781740407757b
Rexter Dayuta <rexterdayuta@gmail.com>
2021-09-08 01:09:01 +0800

    ccs version

--- application/views/header.php
+++ application/views/header.php
@@ -28,13 +28,13 @@ else
         <link href="<?= base_url() ?>front_assets/vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
 
         <!-- Template base -->
-        <link href="<?= base_url() ?>front_assets/css/theme-base.css?v=5" rel="stylesheet">
+        <link href="<?= base_url() ?>front_assets/css/theme-base.css?v=6" rel="stylesheet">
 
         <!-- Template elements -->
         <link href="<?= base_url() ?>front_assets/css/theme-elements.css" rel="stylesheet">
 
         <!-- Responsive classes -->
-        <link href="<?= base_url() ?>front_assets/css/responsive.css" rel="stylesheet">
+        <link href="<?= base_url() ?>front_assets/css/responsive.css?v=1" rel="stylesheet">
 
         <!-- [if lt IE 9]>
         <script src="https://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
