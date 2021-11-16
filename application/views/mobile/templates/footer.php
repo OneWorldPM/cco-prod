3140229557413bbd57ec1c3b9265f1173dfa51fd
Rexter Dayuta <rexterdayuta@gmail.com>
2021-11-17 03:15:20 +0800

    contact details

--- application/views/mobile/templates/footer.php
+++ application/views/mobile/templates/footer.php
@@ -1,2 +1,59 @@
 </body>
 </html>
+
+<script>let pubnub_channel_total = "CCO_Total";</script>
+<script src="<?= base_url() ?>front_assets/js/pubnub/pubnub_total_live_users.js?v=2"></script>
+<script>
+    var user_id = <?= $this->session->userdata("cid") ?>;
+    var user_name = "<?= $this->session->userdata('fullname') ?>";
+    function extract(variable) {
+        for (var key in variable) {
+            window[key] = variable[key];
+        }
+    }
+
+
+
+    $(function() {
+
+        $.get( "<?=base_url()?>socket_config.php", function( data ) {
+            var config = JSON.parse(data);
+            extract(config);
+
+            var socketServer = "https://socket.yourconference.live:443";
+            let socket = io(socketServer);
+            socket.on('serverStatus', function (data) {
+                socket.emit('addMeToActiveListPerApp', {'user_id':user_id, 'app': socket_app_name, 'room': socket_active_user_list});
+            });
+
+            $(window).on("blur focus", function(e) {
+                var prevType = $(this).data("prevType");
+
+                if (prevType != e.type) {   //  reduce double fire issues
+                    switch (e.type) {
+                        case "blur":
+                            inActive();
+                            break;
+                        case "focus":
+                            resetActive();
+                            break;
+                    }
+                }
+
+                $(this).data("prevType", e.type);
+            });
+
+        });
+
+        // Active again
+        function resetActive(){
+            socket.emit('userActiveChangeInApp', {"app":socket_app_name, "room":socket_active_user_list, "name":user_name, "userId":user_id, "status":true});
+        }
+        // No activity let everyone know
+        function inActive(){
+            socket.emit('userActiveChangeInApp', {"app":socket_app_name, "room":socket_active_user_list, "name":user_name, "userId":user_id, "status":false});
+        }
+
+
+    });
+</script>
