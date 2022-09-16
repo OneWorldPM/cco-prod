5b06bf7c87d9b3e2548cdcfe11e547bd80a93059
Rexter Dayuta <rexterdayuta@gmail.com>
2022-09-16 19:06:10 +0800

    onsite poll

--- application/views/view_sessions_optimized_onsite.php
+++ application/views/view_sessions_optimized_onsite.php
@@ -27,7 +27,7 @@
                                 <div class="modal-body view-session-user-modal-body" style="padding: 0px; content" id="content">
                                     <div class="row" style="padding-top: 0px; padding-bottom: 20px; margin-left:5px">
                                         <div class="col-sm-12 col-md-12">
-                                            <div class="row" id="rowLivePoll" style="margin-left:10px;text-align:left; font-size: 20px; font-weight: 700; margin-top:10px;display: none;"> <img src="<?=base_url()?>front_assets/images/cco_sm.png" width="70px"><span>LIVE POLL</span>
+                                            <div class="row" id="rowLivePoll" style="margin-left:10px;text-align:left; font-size: 20px; font-weight: 700; margin-top:10px;display: none;"> <img src="<?=base_url()?>front_assets/images/cco_sm.png" height="25px"><span>LIVE POLL</span>
                                                 <span class="" id=""  style=" font-size: 20px; font-weight: 700; border-top-right-radius: 15px; border-top-left-radius: 15px;  ">
                                                 <span id="timer_sectiom" style="display: none !important; text-align:right; float:right; padding-right:15px; font-size: 20px; font-weight: 700;">Time remaining : <span id="id_day_time" style=" text-align: right;font-size: 20px; font-weight: 700; color: #ef5e25; padding: 0px 10px 0px 0px;"></span></span>
                                             </span></div>
