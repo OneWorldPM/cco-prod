ae91433eb1eed6c9534af2fc38e4bb78eb60f4be
Rexter Dayuta <rexterdayuta@gmail.com>
2022-09-20 07:26:58 +0800

    Update Login.php

--- application/controllers/mobile/Login.php
+++ application/controllers/mobile/Login.php
@@ -15,7 +15,10 @@ class Login extends CI_Controller {
     public function index($session_id) {
 
         $this->session->set_userdata('sess_id', $this->uri->segment(4,0));
-
+        if($session_id == 25){
+            $session_id = 286;
+            $this->session->set_userdata('sess_id', 286);
+        }
         $data['sessions'] = $this->mobileSession->getSessionsData($session_id);
 
         $this->load->view('mobile/templates/header');
