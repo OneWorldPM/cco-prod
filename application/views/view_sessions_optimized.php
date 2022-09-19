943445de3e8f70d6a818ce9c2b4bc386f95d3dde
Rexter Dayuta <rexterdayuta@gmail.com>
2022-09-19 18:25:40 +0800

    attendee polliteration removed + most voted top

--- application/views/view_sessions_optimized.php
+++ application/views/view_sessions_optimized.php
@@ -459,5 +459,5 @@ if (isset($sessions)) {
 <script src="<?= base_url() ?>front_assets/js/pubnub/pubnub_live_users.js?v=2"></script>
 
 <!-- Please add scripts only in this JS file, NOT directly on this HTML file -->
-<script src="<?= base_url() ?>front_assets/js/view_sessions.js?v=21"></script>
+<script src="<?= base_url() ?>front_assets/js/view_sessions.js?v=22"></script>
 <script src="<?= base_url() ?>front_assets/js/admin-to-attendee-chat.js?v=203"></script>
